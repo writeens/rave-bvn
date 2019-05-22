aa2f3b7f9fea4df937ea3f03ef8e8ebe369c62be
writeens <44687555+writeens@users.noreply.github.com>
2019-05-22 16:50:55 +0100

    Updates

---
+++ precache-manifest.febc587f3492a7bfa59761dcc180ebab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d5ff3c1528ee385f98ba4c0bd6ad523",
+    "url": "/rave-bvn/index.html"
+  },
+  {
+    "revision": "5052cec6103a7dd665a0",
+    "url": "/rave-bvn/static/css/main.758b8bda.chunk.css"
+  },
+  {
+    "revision": "6f450d2a2f4a271b6b98",
+    "url": "/rave-bvn/static/js/2.4aa89465.chunk.js"
+  },
+  {
+    "revision": "5052cec6103a7dd665a0",
+    "url": "/rave-bvn/static/js/main.b86107f2.chunk.js"
+  },
+  {
+    "revision": "5130bcad0b65da608a67",
+    "url": "/rave-bvn/static/js/runtime~main.08af0c94.js"
+  }
+]);
