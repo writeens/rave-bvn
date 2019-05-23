e9dfd217c0c9ebd0ee7ea4482ea90a830179d921
writeens <44687555+writeens@users.noreply.github.com>
2019-05-23 07:15:26 +0100

    Updates

--- precache-manifest.0acfa5922dda473631edf96c876f5f58.js
+++ precache-manifest.0acfa5922dda473631edf96c876f5f58.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4f306d0a45822e7285a945c6f57cfd06",
+    "revision": "4947813df178ec8ea024faa7ef483c8b",
     "url": "/rave-bvn/index.html"
   },
   {
-    "revision": "59b0f9e557970e5f88b8",
-    "url": "/rave-bvn/static/css/main.758b8bda.chunk.css"
+    "revision": "12bd7b48cad3427d5489",
+    "url": "/rave-bvn/static/css/main.0196bf02.chunk.css"
   },
   {
     "revision": "6f450d2a2f4a271b6b98",
     "url": "/rave-bvn/static/js/2.4aa89465.chunk.js"
   },
   {
-    "revision": "59b0f9e557970e5f88b8",
-    "url": "/rave-bvn/static/js/main.a77b2a94.chunk.js"
+    "revision": "12bd7b48cad3427d5489",
+    "url": "/rave-bvn/static/js/main.a606137a.chunk.js"
   },
   {
     "revision": "5130bcad0b65da608a67",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rave-bvn/precache-manifest.2def59394ba5b50b8050d61a49ae234c.js"
+  "/rave-bvn/precache-manifest.0acfa5922dda473631edf96c876f5f58.js"
 );
 
 self.addEventListener('message', (event) => {
